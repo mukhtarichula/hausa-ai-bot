b4b983c1794a364a36706178a5a275be07a34698
mukhtarichula <mukhtarichula@gmail.com>
2026-08-29 00:07:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+
+import os
+import threading
 import telebot
+from flask import Flask
 
 TELEGRAM_TOKEN = "8662812194:AAHQcaN89G9vv8uQNWpiSjgCJuAwWMwg4ns"
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
@@ -11,5 +15,19 @@ def send_welcome(message):
 def echo_all(message):
     bot.reply_to(message, f"Na samu saqonka: {message.text}")
 
-if __name__ == "__main__":
+def run_bot():
     bot.infinity_polling(skip_pending=True)
+
+# Fara bot a background thread
+threading.Thread(target=run_bot, daemon=True).start()
+
+# Flask Web Server don Render Web Service
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot yana aiki lafiya!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
